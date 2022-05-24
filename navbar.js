0e49f4d8d2d6e21f3e14f7b8ceea11c06dde76d5
MSchmidt951 <m.c.schmidt951@gmail.com>
2022-05-24 16:12:45 +0100

    Initial commit

---
+++ navbar.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	//Load the navbar
+	$('#navigation').load("/navbar.html");
+	
+	//Add headings to the navbar
+	headings = $("h1");
+	for (var i=0; i<headings.length; i++) {
+		$('#navbar-content').append('<li class="nav-item"><a class="nav-link" href="#' + headings[i].id + '">' + headings[i].textContent + '</a></li>');
+	}
+	
+	//Add an offset when clicking the navbar headings
+	$(function($){
+		$('a[href*="#"]:not([href="#"])').click(function(){
+			var target = $(this.hash);
+			$('html, body').stop().animate({
+				scrollTop: target.offset().top - 64
+			}, 'linear');   
+		});
+	});
+});
